3a137df7816de6791f7ca81b24fe50e83e6e4c48
wyg <wyg129642@sjtu.edu.cn>
2024-06-16 13:38:25 +0800

    finish List

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,20 @@
-public class ArrayDeque {
+/** Array based list.
+ *  @author WYG
+ */
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+    }
+    public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public void addFirst(T item){
+        items[]
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,4 +1,9 @@
-public class LinkedListDeque<T> {
+
+
+/**  first part of project1A.
+ *   Deque implemented by Linked List
+ *  @author wyg
+ */public class LinkedListDeque<T> {
     public class Node {
         private T item;
         private Node next;
@@ -11,16 +16,16 @@ public class LinkedListDeque<T> {
     }
     private Node sentinel;
     private int size;
-    public LinkedListDeque(){
-        sentinel = new Node(null, null);//需要修复
-        sentinel.prev=sentinel;
-        sentinel.next=sentinel;
+    public LinkedListDeque() {
+        sentinel = new Node(null, null); //需要修复
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
         size = 0;
     }
-    public boolean isEmpty(){
-        return size==0;
+    public boolean isEmpty() {
+        return size == 0;
     }
-    public int size(){
+    public int size() {
         return size;
     }
     public void addFirst(T item) {
@@ -32,59 +37,58 @@ public class LinkedListDeque<T> {
 
     public void addLast(T item) {
         size += 1;
-        sentinel.prev.next= new Node(item, sentinel);
+        sentinel.prev.next = new Node(item, sentinel);
         sentinel.prev.next.prev = sentinel.prev;
-        sentinel.prev=sentinel.prev.next;
+        sentinel.prev = sentinel.prev.next;
     }
-    public T removeFirst(){
-        if (isEmpty()){
+    public T removeFirst() {
+        if (isEmpty()) {
             return null;
-        }else{
+        } else {
             size--;
-            T front =sentinel.next.item;
-            sentinel.next=sentinel.next.next;
-            sentinel.next.prev=sentinel;
+            T front = sentinel.next.item;
+            sentinel.next = sentinel.next.next;
+            sentinel.next.prev = sentinel;
             return front;
         }
     }
-    public T removeLast(){
-        if (isEmpty()){
+    public T removeLast() {
+        if (isEmpty()) {
             return null;
-        }else{
+        } else {
             size--;
-            T back=sentinel.prev.item;
-            sentinel.prev=sentinel.prev.prev;
-            sentinel.prev.next=sentinel;
+            T back = sentinel.prev.item;
+            sentinel.prev = sentinel.prev.prev;
+            sentinel.prev.next = sentinel;
             return back;
         }
     }
-    public T get(int index){
-        if (index>=size){
+    public T get(int index) {
+        if (index >= size) {
             return null;
         }
-        Node ptr=sentinel.next;
-        while(index!=0){
-            ptr=ptr.next;
+        Node ptr = sentinel.next;
+        while (index != 0) {
+            ptr = ptr.next;
             index--;
         }
         return ptr.item;
     }
-    public void printDeque(){//感觉有问题
-        while(sentinel.next.item != null){
-            System.out.print(sentinel.item+" ");
-            sentinel = sentinel.next;
-        }
-    }
-
     public T getRecursive(int index) {
-            return getRecursive_help(sentinel, index);
+        return getRecursiveHelp(sentinel, index);
     }
-    public T getRecursive_help(Node ptr, int index){
-        if (index==0){
+    public T getRecursiveHelp(Node ptr, int index) {
+        if (index == 0) {
             return ptr.next.item;
+        } else {
+            return getRecursiveHelp(ptr.next, --index);
         }
-        else{
-            return getRecursive_help(ptr.next, --index);
+    }
+    public void printDeque() {
+        Node ptr = sentinel.next;
+        while (ptr != sentinel) {
+            System.out.print(ptr.item + " ");
+            ptr = ptr.next;
         }
     }
 }
